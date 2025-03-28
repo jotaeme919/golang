6f6b4555b8d3565b239d31620d7f20257067007c
jotaeme919 <joao.m.nascimento17@aluno.senai.br>
2025-03-28 09:22:13 -0300

    Exercício2

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
-    var a int
-	var b int
- 	 fmt.Print("Digite um número")
- 	 fmt.Scan(&a)
-	 fmt.Print("Digite outro número")
-	 fmt.Scan(&b)
-	 fmt.Println("A soma é: ", a + b)
-	 fmt.Println("A subtração é: ", a - b)
-	 fmt.Println("A divisão é: ", a / b)
-	 fmt.Println("A multiplicação é: ", a * b)
-	 fmt.Println("O resto da divisão é: ", a % b)
- 	
-}
+	func main() {
+		var usuario string
+		var senha string
+
+		fmt.Printf("Digite seu usuário e uma senha:")
+        fmt.Scan(&usuario)
+        fmt.Scan(&senha)
+        if (usuario == "admin" && senha == "1234"){
+        fmt.Printf("Usuário correto")
+		} else {
+			fmt.Println("senha incorreta")
+		}
+		
+        
+		
+		}
