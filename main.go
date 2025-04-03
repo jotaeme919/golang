a70b051b7f9e8d0aa0c044a3b82faffebf7b0381
jotaeme919 <joao.m.nascimento17@aluno.senai.br>
2025-04-03 08:57:53 -0300

    Exercício4

--- main.go
+++ main.go
@@ -9,8 +9,22 @@ import "fmt"
 		fmt.Println(nomes)
 		nomes[3] = "Capitão Cueca"
 		fmt.Println(nomes)
-		}
-		
-        
+		// Slice
+		var score = []int{100, 200, 300, 400}
+		fmt.Println(score)
+		score[2] = 333
+		fmt.Println(score, len(score), cap(score))
+		rangeOne := score[1:3]
+		fmt.Println(rangeOne)
+		rangeTwo := score[2:]
+		fmt.Println(rangeTwo)
+	    rangeThree := score[:3]
+		fmt.Println(rangeThree)
+
+		var superherois = []string{"Lanterna-Verde", "Capitão América", "Trunks"}
+		fmt.Println(superherois)
+		superherois = append(superherois, "Asta", "Goku")
+		fmt.Println(superherois, len(superherois), cap(superherois))
+	}
 		
 		
