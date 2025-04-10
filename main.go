daa3a3a9461e0ac8f0d724d9b9b6b4a47b5c8049
jotaeme919 <joao.m.nascimento17@aluno.senai.br>
2025-04-10 11:06:54 -0300

    Saldo

--- main.go
+++ main.go
@@ -5,15 +5,31 @@ import (
 
 )
 func main() {
-	var age int
-	fmt.Println("Digite sua idade: ")
-	fmt.Scan(&age)
-	 if age < 18 {
-		fmt.Println("Você é menor de idade")
-	}else if age <=60{
-		fmt.Println("Você é adulto")
-	} else {
-		fmt.Println("Você é idoso")
+	var saldo int = 20000
+	var saque int
+	var deposito int
+	var opcao string = "deposito || saque"
+    fmt.Println("Olá! Qual das opções você deseja? saque, deposito ou sair? ")
+	fmt.Scan(&opcao)
+	if opcao == "saque" {
+		fmt.Println("Quanto deseja sacar?")
+		fmt.Scan(&saque)
+		saldo -= saque
+	} else if opcao == "deposito" {
+		fmt.Println("Quanto você deseja depositar? ")
+		fmt.Scan(&deposito)
+		saldo += deposito
+	} else  {
+		fmt.Println("Você saiu da sua conta")
+    }
+	fmt.Println("Saldo atual: ", saldo)
+          
+
+
+	
 }
 
-}
+	
+
+
+
