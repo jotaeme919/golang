5ab67228f694214a7b53050268c949cc3deb4bde
jotaeme919 <joao.m.nascimento17@aluno.senai.br>
2025-04-24 14:19:13 -0300

    Map Capitais

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt" )
-
-func pegarNome() (string, string){
-	return "Almofa", "dinha"
-
-}
+	"fmt" )   
+	
 func main() {
-	nome, sobrenome := pegarNome()
-    fmt.Println(nome)
-	fmt.Println(sobrenome)
+    capitais := map[string]string{
+		"SP" : "São Paulo",
+		"RJ" : "Rio de Janeiro",
+		"ES" : "Espírito Santo",
+        "AC" : "Acre",
+	}
+	capitais["BH"] = "Belo Horizonte"
 
- 
+	for k,v := range capitais {
+		fmt.Println("Sigla , Nome", k, v)
+	}
+	delete(capitais, "AC")
+	 for k,v := range capitais {
+		fmt.Println("Sigla, Nome", k, v)
+	 }
 
 }
