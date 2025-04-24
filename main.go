3aa2e7666d131f4e56e559390b757d8c5a168377
jotaeme919 <joao.m.nascimento17@aluno.senai.br>
2025-04-24 13:50:58 -0300

    Função Notas

--- main.go
+++ main.go
@@ -3,24 +3,14 @@ package main
 import (
 	"fmt" )
 
+func pegarNome() (string, string){
+	return "Almofa", "dinha"
+
+}
 func main() {
-	alunoIdade := make(map[string]int)
-	alunoIdade["Bruno"] = 16 
-	alunoIdade["Otávio"] = 16
-	alunoIdade["JM"] = 16
-	alunoIdade["Lele"] = 15
-	fmt.Println("Idade do Bruno", alunoIdade["Bruno"])
-	notasAlunos := map[string]float64{
-		"Bruno" : 1.7,
-		"Otávio" : 7.5,
-		"JM" : 9.8,
-		"Lele" : 10,
-} 
- for k, v := range notasAlunos{
-	fmt.Printf("%s tirou a nota %.1f \n", k, v)
-
-	}
-
+	nome, sobrenome := pegarNome()
+    fmt.Println(nome)
+	fmt.Println(sobrenome)
 
  
 
